7410f51a20e446a4f4648eec18911e5b0a0471a9
Sebastian <s.arriagada@live.com>
2022-09-15 18:22:52 -0400

    Creando programa de encriptacion

---
+++ script.js
@@ -0,0 +1,66 @@
+const inputTexto = document.querySelector(".input-texto")
+const mensaje = document.querySelector(".mensaje")
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = "";
+}
+
+function encriptar(stringEncriptado) {
+    //`La letra "e" es convertida para "enter"`
+    //`La letra "i" es convertida para "imes"`
+    //`La letra "a" es convertida para "ai"`
+    //`La letra "o" es convertida para "ober"`
+    //`La letra "u" es convertida para "ufat"`
+
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado;
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(inputTexto.value)
+    mensaje.value = textoEncriptado
+    inputTexto.value = ""
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+
+}
+
+function copiar() {
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value);
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
