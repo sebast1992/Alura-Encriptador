fb765c7c42190a627f761cb0b20d0473103882b8
Sebastian <s.arriagada@live.com>
2022-09-21 20:29:38 -0400

    Arreglo menor

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function btnEncriptar() {
 }
 
 function encriptar(stringEncriptado) {
-    //`La letra "e" es convertida para "enter"`
-    //`La letra "i" es convertida para "imes"`
-    //`La letra "a" es convertida para "ai"`
-    //`La letra "o" es convertida para "ober"`
-    //`La letra "u" es convertida para "ufat"`
+    //La letra "e" es convertida para "enter"
+    //La letra "i" es convertida para "imes"
+    //La letra "a" es convertida para "ai"
+    //La letra "o" es convertida para "ober"
+    //La letra "u" es convertida para "ufat"
 
     let matrizCodigo = [
         ["e", "enter"],
